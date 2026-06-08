80d220aa1cc3afc785fb706a3564f0c550fe07fd
John Doe <1929897318@qq.com>
2026-06-08 13:49:54 +0800

    商户查询缓存业务代码

--- src/main/java/com/hmdp/service/IShopTypeService.java
+++ src/main/java/com/hmdp/service/IShopTypeService.java
@@ -1,7 +1,10 @@
 package com.hmdp.service;
 
+import com.baomidou.mybatisplus.core.conditions.interfaces.Func;
 import com.hmdp.entity.ShopType;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopTypeService extends IService<ShopType> {
 
+
+    List<ShopType> queryAll();
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +29,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public List<ShopType> queryAll() {
+        String key = RedisConstants.CACHE_SHOP_TYPE_KEY;
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isNotBlank(shopTypeJson)){
+            return JSONUtil.toList(shopTypeJson, ShopType.class);
+        }
+
+        // 不存在，去数据库查询
+        List<ShopType> shopTypes = query()
+                .orderByAsc("sort")
+                .list();
+
+        if (shopTypes == null || shopTypes.isEmpty()){
+            return shopTypes;
+        }
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shopTypes));
+        return shopTypes;
+    }
 }
